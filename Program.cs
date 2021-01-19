65d1948f2125f14a506d2c03afb9ca78621a9fc2
Mark Brighton <marik.shevchenko@icloud.com>
2021-01-19 17:01:48 +0200

    Final version of this project.

--- Program.cs
+++ Program.cs
@@ -10,39 +10,47 @@ namespace LinkedListLesson
     {
         static void Main(string[] args)
         {
-
+            // Create linked list
             var list = new Model.LinkedList<int>();
 
+            // Add data to list
             list.Add(1);
             list.Add(2);
             list.Add(3);
             list.Add(4);
             list.Add(5);
 
+            // Show list
             foreach (var item in list)
             {
                 Console.Write(item + " ");
             }
             Console.WriteLine();
 
+            //Delete element from list
             list.Delete(3);
 
+            // Show list
             foreach (var item in list)
             {
                 Console.Write(item + " ");
             }
             Console.WriteLine();
 
+            // Add new element to head of list
             list.AppendHead(7);
 
+            // Show list
             foreach (var item in list)
             {
                 Console.Write(item + " ");
             }
             Console.WriteLine();
 
+            // Insert new element after target element
             list.InsertAfter(4, 77);
 
+            // Show list
             foreach (var item in list)
             {
                 Console.Write(item + " ");
